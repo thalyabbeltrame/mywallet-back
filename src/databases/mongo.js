d53f0bd80dc3dbad68441d4ee28257472df9ef3d
Thalya Busato Beltrame <beltrame.thalya@gmail.com>
2022-07-03 14:16:24 -0300

    chore: adjust code for deploy on heroku

--- src/databases/mongo.js
+++ src/databases/mongo.js
@@ -4,7 +4,7 @@ import chalk from 'chalk';
 
 dotenvConfig();
 
-const MONGO_URI = process.env.MONGO_URL || 'mongodb://localhost:27017';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
 const DATABASE = process.env.DATABASE || 'my-wallet';
 
 let database = null;
